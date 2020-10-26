dd7b47678dd52396eac94202c36448c691e1d1cc
Claude Ebaneck <claude.ebaneck@scality.com>
2020-10-26 10:32:04 +0100

    Add a `variable.tf` file to abstract variables to a common place

---
+++ cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "GP1-M"
+}
+
+variable "disk_size_gb" {
+    default = "550"
+}
+
+variable "image_type" {
+    default = "ubuntu_focal"
+}
+
+variable "cluster_name" {
+    default = "default"
+}
